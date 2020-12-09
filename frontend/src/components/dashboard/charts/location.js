3213a05a2dee945f8a309021855cabc64542b657
Hüseyin Bera Bulut <berabulut@gmail.com>
2020-12-09 23:44:14 +0300

    map update

--- frontend/src/components/dashboard/charts/location.js
+++ frontend/src/components/dashboard/charts/location.js
@@ -1,37 +1,68 @@
 import React from "react";
 import { WorldMap } from "react-svg-worldmap";
-import { withStyles } from "@material-ui/core";
+import { withStyles, Typography } from "@material-ui/core";
 import "./world-map.css";
 
 const pageStyles = (theme) => ({
   componentContainer: {
-	width: "100%",
-	height: "100%",
+    width: "100%",
+    height: "100%",
     margin: "auto",
   },
   title: {
     color: "white",
-    marginTop: "15%",
+    marginTop: "5.5%",
+    marginBottom: "3.5%",
     fontWeight: "700",
-  }
+  },
 });
 
 const LocationMap = (props) => {
   const { classes } = props;
-  const data = [
-    { country: "tr", value: 1389618778 }, // china
-    { country: "us", value: 0 }, // india
-  ];
+  const [data, setData] = React.useState([
+    { country: "mx", value: "Tacubaya / Mexico City" },
+    { country: "us", value: null },
+  ]);
+  const [fetched, setFetched] = React.useState([]);
+
+  React.useEffect(() => {
+    if (props.locationData !== undefined) {
+      setData([
+        {
+          country: props.locationData.countryCode,
+          value:
+            props.locationData.location !== undefined &&
+            props.locationData.location.substring(
+              0,
+              props.locationData.location.indexOf(",")
+            ),
+        },
+        { country: "us", value: null },
+      ]);
+      setTimeout(() => {
+        setFetched(true);
+      }, 3500);
+    }
+  }, [props]);
 
   return (
     <div className={(classes.componentContainer, "worldmap-container")}>
-      <WorldMap
-	  	className="xxs"
-        color="#4d5aa5"
-        value-suffix="people"
-        size="lg"
-		data={data}
-      />
+      {fetched === true && (
+        <>
+          <Typography className={classes.title} variant="h4">
+            {props.locationData !== undefined
+              ? props.locationData.location
+              : "SOMEWHERE"}
+          </Typography>
+          <WorldMap
+            className="xxs"
+            color="#4d5aa5"
+            value-suffix="people"
+            size="lg"
+            data={data}
+          />
+        </>
+      )}
     </div>
   );
 };
--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -3,6 +3,7 @@ import { withStyles, Container, Grid, Divider } from "@material-ui/core";
 import TemperatureChart from "./charts/temperature";
 import HumidityChart from "./charts/humidity";
 import LocationMap from "./charts/location";
+import { fetchLocation } from "../../api";
 
 const pageStyles = (theme) => ({
   componentContainer: {
@@ -14,15 +15,43 @@ const pageStyles = (theme) => ({
   },
   divider: {
     backgroundColor: "white",
+    width: "100%",
+    marginTop: "3.5%"
   },
   worldmapGrid: {
     width: "580px",
-    height: "450px"
+    height: "650px"
   }
 });
 
 const Dashboard = (props) => {
   const { classes } = props;
+  const [locationData, setLocationData] = React.useState({});
+
+  React.useEffect(() => {
+    let isMounted = true;
+    let formJSON = JSON.stringify({
+      deviceID: props.deviceID
+    });
+    fetchLocation(formJSON)
+    .then((value) => {
+      if(value.statusCode === 200 && isMounted) {
+        setLocationData({
+          countryCode: value.countryCode,
+          location: value.location
+        })
+      }
+      else {
+        if(isMounted) {
+          setLocationData({
+            countryCode: "UK",
+            location: "Greenwich / London"
+          })
+        }
+      }
+    })
+  }, [])
+
   return (
     <Container className={classes.componentContainer} maxWidth="lg">
       <Grid container spacing={3}>
@@ -32,8 +61,9 @@ const Dashboard = (props) => {
         <Grid item sm={6} xs={12}>
           <HumidityChart />
         </Grid>
+        <Divider className={classes.divider} />
         <Grid className={classes.worldmapGrid} item xs={12}>
-          <LocationMap />
+          <LocationMap locationData={locationData} />
         </Grid>
       </Grid>
       <Divider className={classes.divider} />
